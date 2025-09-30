22c73431f3d0b4606abee7ff8a3c576e726bae3c
Lingjie Ye <22338022@zju.edu.cn>
2025-10-01 00:11:37 +0800

    提交obspy_test

---
+++ z_obspy_test/obspy-clients-fdsn-2.py
@@ -0,0 +1,9 @@
+from obspy import UTCDateTime
+from obspy.clients.fdsn import Client
+client = Client("USGS")
+starttime = UTCDateTime("2020-01-01")
+endtime = UTCDateTime("2020-01-02")
+cat = client.get_events(starttime=starttime, endtime=endtime,
+                        minmagnitude=5.0)
+print(cat)
+# cat.plot()
---
+++ z_obspy_test/obspy-clients-fdsn-3.py
@@ -0,0 +1,10 @@
+from obspy import UTCDateTime
+from obspy.clients.fdsn import Client
+client = Client()
+starttime = UTCDateTime("2002-01-01")
+endtime = UTCDateTime("2002-01-02")
+inventory = client.get_stations(network="IU", station="A*",
+                                starttime=starttime,
+                                endtime=endtime)
+print(inventory)
+inventory.plot()
